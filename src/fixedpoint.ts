b9098c34f09cec6233c8f80459ffa113ba414c01
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-09-24 08:23:18 +0200

    no tsup

--- src/fixedpoint.ts
+++ src/fixedpoint.ts
@@ -1,3 +1,5 @@
+import { formatUnits, parseUnits } from "./utils";
+
 export class FixedInt<T extends number> {
   _value: bigint;
   decimals: T;
@@ -64,62 +66,4 @@ export class FixedPortion<T extends number> extends FixedInt<T> {
   setPercentage(percentage: number): void {
     this.val = parseUnits(percentage.toString(), this.decimals);
   }
-}
-
-// lifted from viem
-function formatUnits(value: bigint, decimals: number) {
-  let display = value.toString();
-
-  const negative = display.startsWith("-");
-  if (negative) display = display.slice(1);
-
-  display = display.padStart(decimals, "0");
-
-  let [integer, fraction] = [
-    display.slice(0, display.length - decimals),
-    display.slice(display.length - decimals),
-  ];
-  fraction = fraction.replace(/(0+)$/, "");
-  return `${negative ? "-" : ""}${integer || "0"}${
-    fraction ? `.${fraction}` : ""
-  }`;
-}
-
-function parseUnits(value: string, decimals: number) {
-  let [integer, fraction = "0"] = value.split(".");
-
-  const negative = integer.startsWith("-");
-  if (negative) integer = integer.slice(1);
-
-  // trim leading zeros.
-  fraction = fraction.replace(/(0+)$/, "");
-
-  // round off if the fraction is larger than the number of decimals.
-  if (decimals === 0) {
-    if (Math.round(Number(`.${fraction}`)) === 1)
-      integer = `${BigInt(integer) + 1n}`;
-    fraction = "";
-  } else if (fraction.length > decimals) {
-    const [left, unit, right] = [
-      fraction.slice(0, decimals - 1),
-      fraction.slice(decimals - 1, decimals),
-      fraction.slice(decimals),
-    ];
-
-    const rounded = Math.round(Number(`${unit}.${right}`));
-    if (rounded > 9)
-      fraction = `${BigInt(left) + BigInt(1)}0`.padStart(left.length + 1, "0");
-    else fraction = `${left}${rounded}`;
-
-    if (fraction.length > decimals) {
-      fraction = fraction.slice(1);
-      integer = `${BigInt(integer) + 1n}`;
-    }
-
-    fraction = fraction.slice(0, decimals);
-  } else {
-    fraction = fraction.padEnd(decimals, "0");
-  }
-
-  return BigInt(`${negative ? "-" : ""}${integer}${fraction}`);
 }
